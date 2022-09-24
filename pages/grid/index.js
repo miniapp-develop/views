1af3f7566668c0bd348e1847e4d80fa5e5f0291a
xesam <xesam@outlook.com>
2022-09-25 00:34:42 +0800

    demo grid /fix

--- pages/grid/index.js
+++ pages/grid/index.js
@@ -1,6 +1,10 @@
 Page({
     data: {
-        config: {
+        config1: {
+            cols: 2,
+            itemCount: 5
+        },
+        config2: {
             minCols: 2,
             maxCols: 3,
             itemCount: 5
@@ -9,35 +13,35 @@ Page({
     onTap(e) {
         console.log('点击了：', e.detail);
     },
-    onTapCtrl(e) {
+    onTapCtrl(e, config) {
         const name = e.target.dataset.name;
         const [target, op] = name.split('.');
         if (op === 'add') {
-            if (target === 'minCols' && this.data.config.minCols + 1 > this.data.config.maxCols) {
-                console.error('minCols 不能大于 maxCols');
+            if (target === 'minCols' && this.data[config].minCols + 1 > this.data[config].maxCols) {
+                console.warn('minCols 不能大于 maxCols');
                 return;
             }
             this.setData({
-                config: {
-                    ...this.data.config,
-                    [target]: this.data.config[target] + 1
-                }
+                [`${config}.${target}`]: this.data[config][target] + 1
             });
         } else if (op === 'minus') {
-            if (this.data.config[target] === 1) {
-                console.error(target + ' 不能小于 1');
+            if (this.data[config][target] === 1) {
+                console.warn(target + ' 不能小于 1');
                 return;
             }
-            if (target === 'maxCols' && this.data.config.maxCols - 1 < this.data.config.minCols) {
-                console.error('maxCols 不能小于 minCols');
+            if (target === 'maxCols' && this.data[config].maxCols - 1 < this.data[config].minCols) {
+                console.warn('maxCols 不能小于 minCols');
                 return;
             }
             this.setData({
-                config: {
-                    ...this.data.config,
-                    [target]: this.data.config[target] - 1
-                }
+                [`${config}.${target}`]: this.data[config][target] - 1
             });
         }
+    },
+    onTapCtrlConfig1(e) {
+        this.onTapCtrl(e, 'config1');
+    },
+    onTapCtrlConfig2(e) {
+        this.onTapCtrl(e, 'config2');
     }
 })
