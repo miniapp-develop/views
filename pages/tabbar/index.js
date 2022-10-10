8f7fb4cf1d13725be53b424f44ea4d743047703e
xesam <xesam@outlook.com>
2022-10-10 22:16:35 +0800

    tabbar /fix

--- pages/tabbar/index.js
+++ pages/tabbar/index.js
@@ -11,32 +11,17 @@ Page({
             'config1.activeId': e.detail.id
         });
     },
-    onTapCtrl(e, config) {
-        const name = e.target.dataset.name;
-        const [target, op] = name.split('.');
-        if (op === 'add') {
-            if (target === 'minCols' && this.data[config].minCols + 1 > this.data[config].maxCols) {
-                console.warn('minCols 不能大于 maxCols');
-                return;
-            }
-            this.setData({
-                [`${config}.${target}`]: this.data[config][target] + 1
-            });
-        } else if (op === 'minus') {
-            if (this.data[config][target] === 1) {
-                console.warn(target + ' 不能小于 1');
-                return;
-            }
-            if (target === 'maxCols' && this.data[config].maxCols - 1 < this.data[config].minCols) {
-                console.warn('maxCols 不能小于 minCols');
-                return;
-            }
-            this.setData({
-                [`${config}.${target}`]: this.data[config][target] - 1
-            });
-        }
+    onConfigChanged(e, config) {
+        console.log(e)
+        const payload = e.detail.payload;
+        console.log({
+            [`${config}`]: payload.value
+        })
+        this.setData({
+            [`${config}`]: payload.value
+        });
     },
-    onTapCtrlConfig1(e) {
-        this.onTapCtrl(e, 'config1');
+    onConfig1Changed(e) {
+        this.onConfigChanged(e, 'config1.itemCount');
     }
 })
